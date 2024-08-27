4b1106560992da663a21c284eca1310d34dc67c5
FructDev <luisfructuoso86@gmail.com>
2024-08-27 08:58:32 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/DF_Frontend/",
+  //base: "/DF_Frontend/",
   plugins: [react()],
 });
