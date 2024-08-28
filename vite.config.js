8d0f79ea47a44e5e260c329e64f69b2264eaad65
FructDev <luisfructuoso86@gmail.com>
2024-08-27 22:19:06 -0400

    vite.config_modificate 01

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig(({ mode }) => {
-  return {
-    base: mode === "production" ? "/DF_Frontend/" : "/",
-    plugins: [react()],
-  };
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "https://github.com/FructDev/DF_Frontend",
+  plugins: [react()],
 });
