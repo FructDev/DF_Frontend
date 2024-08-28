cb9acc2927bb1239e47370eb57c31afc39d20a94
FructDev <luisfructuoso86@gmail.com>
2024-08-27 22:29:39 -0400

    other intent

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/FructDev/DF_Frontend",
+  base: "https://fructdev.github.io/DF_Frontend/",
   plugins: [react()],
 });
